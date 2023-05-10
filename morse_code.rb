3ff94b983d1562e1412bc93ddfa190fbcb6df1f3
Gmanlove <emmagman3@gmail.com>
2023-05-10 09:41:27 +0100

    Update the morse_code.rb file

--- morse_code.rb
+++ morse_code.rb
@@ -4,3 +4,16 @@ morse_code = {
   'O' => '---', 'P' => '.--.', 'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-', 'U' => '..-',
   'V' => '...-', 'W' => '.--', 'X' => '-..-', 'Y' => '-.--', 'Z' => '--..'
 }
+
+$reverse_morse_code = morse_code.invert
+
+def decode_character code
+  $reverse_morse_code[code]
+end
+puts decode_character "-.-"
+
+def decode_word word_code
+  code_list = word_code.split
+  word = code_list.map {|code| decode_character code}.join("")
+end
+puts decode_word "-- -.--"
